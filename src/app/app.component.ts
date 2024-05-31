e0fe330dc3f70d41378c55035b49ef6a98aeb0ca
Joao Augusto <joaoaugustofrezende@outlook.com>
2024-05-31 15:27:34 -0300

    Site finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, HostListener } from '@angular/core';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { NgFor, NgIf, NgOptimizedImage } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NgOptimizedImage,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatMenuModule,
+    NgIf,
+    RouterLink,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'SiteBorgesESilva';
+  mostraHeader = false;
+  mostraOpcoesMobile = false;
+
+  @HostListener('window:scroll') onScroll() {
+    if (Math.round(window.scrollY) > 450) {
+      this.mostraHeader = true;
+    } else {
+      this.mostraHeader = false;
+    }
+  }
+
+  viewMobileOptions() {
+    this.mostraOpcoesMobile = !this.mostraOpcoesMobile;
+  }
 }
